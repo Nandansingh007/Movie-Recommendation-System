3249440fa24c3ea5cfd8f06e9d71ec5730886c18
nandan <nandan.singhs007@gmail.com>
2023-07-24 14:42:54 +0545

    Frontend development with streamlit

---
+++ frontend/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import requests
+
+
+#Header Display
+st.header('Movie Recommender System')
+
+# Create a text area to take input movie name
+movie_name = st.text_input("Enter a movie name:")
+
+if st.button("Recommend"):
+    pass
+
