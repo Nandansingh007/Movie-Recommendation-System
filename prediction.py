d02c8f69a5889c378090c31677387f81e3932c6b
nandan <nandan.singhs007@gmail.com>
2023-07-24 08:18:33 +0545

    Prediction modified

--- prediction.py
+++ prediction.py
@@ -4,10 +4,8 @@ import pandas as pd
 
 #loading picke model
 similar = pickle.load(open("./Model/similar.pkl", 'rb'))
-movies = pickle.load(open('./model/movies1.pkl','rb'))
+movies = pickle.load(open('./model/movies.pkl','rb'))
 
-#Load cleaned data
-new_df = pd.read_csv("./Dataset/clean_df.csv")
 
 # Creating our Recommend function it will return Top 3 movies back
 # Recommend top similar movies
@@ -17,7 +15,7 @@ def recommend(movie):
     movie_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:4]
 
     for i in movie_list:
-        print(new_df.iloc[i[0]].title)
+        print(movies.iloc[i[0]].title)
 
 """## Testing the result Results"""
 recommend("Batman Begins")
