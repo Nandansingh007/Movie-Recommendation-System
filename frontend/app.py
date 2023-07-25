d8c7a028d72f40c254f4e49facf04dfc3f08be6a
nandan <nandan.singhs007@gmail.com>
2023-07-25 11:09:51 +0545

    Calling backend api when button is clicked

--- frontend/app.py
+++ frontend/app.py
@@ -5,15 +5,38 @@ import pickle
 #import movies model
 movies = pickle.load(open('/app/movies.pkl','rb'))
 
+#port where backend is running
+api_base_url = "http://localhost:8000"
 
 #Header Display
 st.title('Movie Recommender System')
 
 selected_movie_name = st.selectbox(
-'Select Movie',
+'Type or Select Movie From Dropdown',
 movies['title'].values
 )
 
 if st.button("Recommend"):
-    pass
+    
+    # Prepare the API endpoint and parameters
+    api_endpoint = f"{api_base_url}/recommend/"
+    params = {"movie": selected_movie_name}
+
+    # Make the API call to get the recommendations
+    response = requests.get(api_endpoint, params=params)
+
+    if response.status_code == 200:
+        # Successfully received the response from the API
+        data = response.json()
+        input_movie = data["input_movie"]
+        recommended_movies = data["recommended_movies"]
+
+        # Display the recommendation results
+        st.write(f"Input Movie: {input_movie}")
+        st.write("Recommended Movies:")
+        for movie in recommended_movies:
+            st.write(movie)
+    else:
+        # Something went wrong with the API call
+        st.error("Error occurred while fetching recommendations. Please try again.")
 
